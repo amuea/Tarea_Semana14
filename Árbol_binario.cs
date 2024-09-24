bcff6c7fd45559e1a352a9c252a3412e885b2a63
Angela <142706975+amuea@users.noreply.github.com>
2024-09-23 23:26:35 -0500

    Datos primitivos

---
+++ Árbol_binario.cs
@@ -0,0 +1,13 @@
+public class Nodo
+{
+    public int Valor;
+    public Nodo Izquierda;
+    public Nodo Derecha;
+
+    public Nodo(int valor)
+    {
+        Valor = valor;
+        Izquierda = null;
+        Derecha = null;
+    }
+}
